eecf2167857a55864c13753c3d268b248a807345
President <aaron.m.wilson@uconn.edu>
2022-10-28 23:38:18 -0400

    m

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const { ApolloServer, gql } = require('apollo-server')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,9 +9,18 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes.js')(app);
-
-app.listen(PORT, function () {
-  console.log(`Now listening on port: ${PORT}`);
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+    db.once("open", () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(
+          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+        );
+      });
+    });
+  };
+  
+startApolloServer(typeDefs, resolvers);
 
